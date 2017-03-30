40db63976167ade8304e4a87172c05ff5b87c595
Lord Suhuy <corvinus@list.ru>
2017-03-30 12:15:49 +0300

    Visualization was implemented. Next it must be wired up with the conversion progress.

--- CopyDb/Program.cs
+++ CopyDb/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using CopyDb.Data;
 using CopyDb.MetaData;
@@ -14,10 +16,28 @@ namespace CopyDb
 
         private const string MsConStr = "Server=.;Database=ISCommerce2;Trusted_Connection=True;MultipleActiveResultSets=True";
         private const string PgConStr = "User ID=postgres;Password=1364791835Q!;Host=localhost;Port=5432;Database=test;Pooling=false;";
-        
+
         static void Main(string[] args)
         {
-            //todo DB drop/ creation logic
+            //todo: 1 create concurrent dictionary (table -> progress)
+            //todo: 2 update the dictionary during export
+            //todo: 3. show progress in parallel task during export
+
+            //>>>> emulation
+            var ttt = Table.GetTables("ISCommerce2", "dbo", MsConStr);
+            var data = Enumerable.Range(1, 10).Select(p => ttt.ToDictionary(k => k.Name, v => p/(double)10)).ToList();
+
+            Console.WriteLine("Press any key to start");
+            Console.ReadKey();
+            Console.Clear();
+
+            foreach (var step in data)
+            {
+                Print(step, ConsoleColor.DarkGreen);
+                Thread.Sleep(200);
+            }
+            return;
+            //<< emulation
 
             var sw = new Stopwatch();
             sw.Start();
@@ -37,7 +57,7 @@ namespace CopyDb
             });
 
             //create: seq, pk, ix, fk
-            text =  String.Join("\r\n\r\n", tables.SelectMany(x => x.Identities).Select(x => x.Render()));
+            text = String.Join("\r\n\r\n", tables.SelectMany(x => x.Identities).Select(x => x.Render()));
             text += "\r\n\r\n" + String.Join("\r\n\r\n", tables.Select(x => x.PrimaryKey.Render()));
             text += "\r\n\r\n" + String.Join("\r\n\r\n", tables.SelectMany(x => x.Indices).Select(x => x.Render()));
             text += "\r\n\r\n" + String.Join("\r\n\r\n", tables.SelectMany(x => x.ForeignKeys).Select(x => x.Render()));
@@ -50,12 +70,46 @@ namespace CopyDb
         private static void ExecuteCommand(string cmdText)
         {
             using (var con = new NpgsqlConnection(PgConStr))
-            using (var cmd = new NpgsqlCommand(cmdText,con))
+            using (var cmd = new NpgsqlCommand(cmdText, con))
             {
                 con.Open();
                 cmd.ExecuteNonQuery();
                 con.Close();
             }
+        }
+
+        private static void Print(Dictionary<string, double> progress, ConsoleColor color)
+        {
+            Console.SetCursorPosition(0,0);
+            int max = Console.WindowWidth;
+            int position = 0;
+            foreach (var table in progress)
+            {
+                if (position + table.Key.Length + 2 >= max)
+                {
+                    Console.WriteLine();
+                    Write(table.Key, table.Value, color);
+                    position = table.Key.Length + 2;
+                }
+                else
+                {
+                    Write(table.Key, table.Value, color);
+                    position += table.Key.Length + 2;
+                }
+            }
+        }
+
+        private static void Write(string text, double progress, ConsoleColor color)
+        {
+            text = $"[{text}]";
+            var cur = (int)Math.Round(progress * text.Length);
+            var empty = Console.BackgroundColor;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.BackgroundColor = cur > i ? color : empty;
+                Console.Write(text[i]);
+            }
+            Console.BackgroundColor = empty;
         }
     }
 }
